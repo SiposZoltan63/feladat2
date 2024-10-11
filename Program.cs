b541a583d48ec586e97a3af28f6f9d84616a91b5
SiposZoltan63 <siposz@kkszki.hu>
2024-10-11 13:34:35 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feladat23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour < 5)
+            {
+                Console.WriteLine("Jó éjszakát.");
+            }
+            else if (hour < 12)
+            {
+                Console.WriteLine("Jó reggelt.");
+            }
+            else if (hour < 18)
+            {
+                Console.WriteLine("Jó napot.");
+            }
+            else if (hour < 24)
+            {
+                Console.WriteLine("Jó estét.");
+            }
+        }
+        static double megtakaritas;
+        public static void kamat()
+        {
+            double megtakaritas;
+            int betet;
+            int kamat;
+            int ev;
+            Console.WriteLine("Kérem a betét összegét: ");
+            betet = int.Parse(Console.ReadLine());
+            Console.Write("Kérem a kamtot: ");
+            kamat = int.Parse(Console.ReadLine());
+            Console.Write("Kérem a megtakarítás hosszát: ");
+            ev = int.Parse(Console.ReadLine());
+            for (int i = 0;i < ev;i++)
+            {
+                megtakaritas = kamat / betet * 100;
+            }
+            return megtakaritas;
+        }
+    }
+}
